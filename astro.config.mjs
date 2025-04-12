ff488518adb00fe46139553d1df4a5aa48f8cd82
Van Nguyen <anhvandev.it@gmai.com>
2025-04-12 12:51:30 +0700

    feat: add some lib

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import node from '@astrojs/node';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "server",
 
   adapter: node({
     mode: "middleware",
   }),
+
+  integrations: [alpinejs()],
 });
---
+++ src/type.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    interface Window {
+        changeMainImage: any;
+        zoomImage: any;
+        selectColor: any;
+        selectSize: any;
+    }
+}
